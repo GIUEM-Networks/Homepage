787e9b137a365cc8732aed99bcce3c5d322bd397
giuem <giuemcom@gmail.com>
2018-02-23 22:32:14 +0800

    deploy.sh: Fix branch typo

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git config user.name "${GITHUB_USER}"
 git config user.email "${GITHUB_EMAIL}"
 git add .
 git commit -m "Travis CI: Auto build and deploy."
-git push --force --quiet https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git master:pages
+git push --force --quiet https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git master:gh-pages
